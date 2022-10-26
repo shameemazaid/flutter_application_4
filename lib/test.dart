510882866ec7bbc30c8975a599627ac8e6f07ed7
shameemazaid <shameemazaiden7669.com>
2022-10-26 12:44:04 +0530

    aaaa class adeed

---
+++ lib/test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class aaaa extends StatefulWidget {
+  const aaaa({Key? key}) : super(key: key);
+
+  @override
+  State<aaaa> createState() => _aaaaState();
+}
+
+class _aaaaState extends State<aaaa> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
